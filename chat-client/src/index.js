e8adfb0e9b2717aff62836fcb1a8c8bd21659df1
NonRoute <chanon.ratn@gmail.com>
2023-03-26 18:34:08 +0700

    Setup project
    https://developer.okta.com/blog/2021/07/14/socket-io-react-tutorial

---
+++ chat-client/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom'
+import { LoginCallback, Security } from '@okta/okta-react'
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+
+const oktaAuth = new OktaAuth({
+	issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+	clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+	redirectUri: `${window.location.origin}/login/callback`
+})
+
+function SecuredRoutes(props) {
+	const history = useHistory()
+	const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+		history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+	}
+
+	return (
+		<Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+			<Route path="/" exact component={App} />
+			<Route path="/login/callback" component={LoginCallback} />
+		</Security>
+	)
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Router>
+			<SecuredRoutes />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
---
+++ chat-server/chat.js
@@ -0,0 +1,102 @@
+require('dotenv').config()
+const uuidv4 = require('uuid').v4
+const OktaJwtVerifier = require('@okta/jwt-verifier')
+const okta = require('@okta/okta-sdk-nodejs')
+
+const jwtVerifier = new OktaJwtVerifier({
+	clientId: process.env.OKTA_CLIENT_ID,
+	issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`
+})
+
+const oktaClient = new okta.Client({
+	orgUrl: process.env.OKTA_ORG_URL,
+	token: process.env.TOKEN
+})
+
+async function authHandler(socket, next) {
+	const { token = null } = socket.handshake.query || {}
+	if (token) {
+		try {
+			const [authType, tokenValue] = token.trim().split(' ')
+			if (authType !== 'Bearer') {
+				throw new Error('Expected a Bearer token')
+			}
+
+			const {
+				claims: { sub }
+			} = await jwtVerifier.verifyAccessToken(tokenValue, 'api://default')
+			const user = await oktaClient.getUser(sub)
+
+			users.set(socket, {
+				id: user.id,
+				name: [user.profile.firstName, user.profile.lastName].filter(Boolean).join(' ')
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	next()
+}
+
+const messages = new Set()
+const users = new Map()
+
+const defaultUser = {
+	id: 'anon',
+	name: 'Anonymous'
+}
+
+const messageExpirationTimeMS = 5 * 60 * 1000
+
+class Connection {
+	constructor(io, socket) {
+		this.socket = socket
+		this.io = io
+
+		socket.on('getMessages', () => this.getMessages())
+		socket.on('message', (value) => this.handleMessage(value))
+		socket.on('disconnect', () => this.disconnect())
+		socket.on('connect_error', (err) => {
+			console.log(`connect_error due to ${err.message}`)
+		})
+	}
+
+	sendMessage(message) {
+		this.io.sockets.emit('message', message)
+	}
+
+	getMessages() {
+		messages.forEach((message) => this.sendMessage(message))
+	}
+
+	handleMessage(value) {
+		const message = {
+			id: uuidv4(),
+			user: users.get(this.socket) || defaultUser,
+			value,
+			time: Date.now()
+		}
+
+		messages.add(message)
+		this.sendMessage(message)
+
+		setTimeout(() => {
+			messages.delete(message)
+			this.io.sockets.emit('deleteMessage', message.id)
+		}, messageExpirationTimeMS)
+	}
+
+	disconnect() {
+		users.delete(this.socket)
+	}
+}
+
+function chat(io) {
+	io.use(authHandler)
+	io.on('connection', (socket) => {
+		new Connection(io, socket)
+	})
+}
+
+module.exports = chat
